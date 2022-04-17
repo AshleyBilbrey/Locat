094a3fdb01aecfe4c8cea41533604b8c5782ce4a
Karim Abou Najm <kabounajm@ucdavis.edu>
2022-04-17 00:16:17 -0700

    implemented model for finding cat contour

---
+++ identifier/model1/inference.py
@@ -0,0 +1,83 @@
+import os
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from model.src.models.modnet import MODNet
+
+def predict(im, output_path, ckpt_path):
+    # define hyper-parameters
+    ref_size = 512
+
+    # define image to tensor transform
+    im_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+
+    # create MODNet and load the pre-trained ckpt
+    modnet = MODNet(backbone_pretrained=False)
+    modnet = nn.DataParallel(modnet)
+
+    if torch.cuda.is_available():
+        modnet = modnet.cuda()
+        weights = torch.load(ckpt_path)
+    else:
+        weights = torch.load(ckpt_path, map_location=torch.device('cpu'))
+    modnet.load_state_dict(weights)
+    modnet.eval()
+
+    # inference images
+    if im:
+        # unify image channels to 3
+        im = np.asarray(im)
+        if len(im.shape) == 2:
+            im = im[:, :, None]
+        if im.shape[2] == 1:
+            im = np.repeat(im, 3, axis=2)
+        elif im.shape[2] == 4:
+            im = im[:, :, 0:3]
+
+        # convert image to PyTorch tensor
+        im = Image.fromarray(im)
+        im = im_transform(im)
+
+        # add mini-batch dim
+        im = im[None, :, :, :]
+
+        # resize image for im
+        im_b, im_c, im_h, im_w = im.shape
+        if max(im_h, im_w) < ref_size or min(im_h, im_w) > ref_size:
+            if im_w >= im_h:
+                im_rh = ref_size
+                im_rw = int(im_w / im_h * ref_size)
+            elif im_w < im_h:
+                im_rw = ref_size
+                im_rh = int(im_h / im_w * ref_size)
+        else:
+            im_rh = im_h
+            im_rw = im_w
+
+        im_rw = im_rw - im_rw % 32
+        im_rh = im_rh - im_rh % 32
+        im = F.interpolate(im, size=(im_rh, im_rw), mode='area')
+
+        # inference
+        _, _, matte = modnet(im.cuda() if torch.cuda.is_available() else im, True)
+
+        # resize and save matte
+        matte = F.interpolate(matte, size=(im_h, im_w), mode='area')
+        matte = matte[0][0].data.cpu().numpy()
+        matte_name = "im_ndame.split('.')[0]+.png"
+        Image.fromarray(((matte * 255).astype('uint8')), mode='L').save(os.path.join(output_path, matte_name))
+        # return Image.fromarray(((matte * 255).astype('uint8')), mode='L')
+        # final = Image.fromarray(((matte * 255).astype('uint8')), mode='L')
+        # buffer = BytesIO()
+        # final.save(buffer, format="jpeg")
+        # return buffer
