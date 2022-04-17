c556929a679e484aac7165286435ec046f44c672
tim <timstewartj@gmail.com>
2022-04-17 01:15:04 -0700

    update hamburger and infobox pawnail and other ui

--- index.js
+++ index.js
@@ -21,10 +21,13 @@ app.get("/map", function(req, res){
     // all data that needs to go to the map page will be stored in this mapdata object
     const mapData = constructMap();
 
+    const cheetoImg = "https://www.sacbee.com/latest-news/xz27im/picture231405683/alternates/FREE_1140/CHEETO.jpg";
+    const testImg = "https://magazine.ucdavis.edu/wp-content/uploads/2019/09/Cheeto-web-header.jpg"
+
     mapData.catMarkers = [
-        { id: "jjfjdjeeee", name: "Cheeto", imgUrl: "https://localwiki.org/media/cache/8a/5c/8a5cee3ca2abc90ce86363a595e8222a.png", lat: 38.5449, lng: -121.7405 },
-        { id: "jee333nendje", name: "Joe the Cat", imgUrl: "", lat: 38.5469, lng: -121.7465 },
-        { id: "udjj3j3jjj", name: "boebinga", imgUrl: "", lat: 38.5369, lng: -121.7565 },
+        { _id: "jjfjdjeeee", name: "Cheeto", imgUrl: cheetoImg, lat: 38.5449, lng: -121.7405, canpet: true, canfeed: false, healthy: false },
+        { _id: "jee333nendje", name: "Joe the Cat", imgUrl: testImg, lat: 38.5469, lng: -121.7465, canfeed: true },
+        { _id: "udjj3j3jjj", name: "boebinga", imgUrl: "", lat: 38.5369, lng: -121.7565, healthy: true },
     ]
 
     res.render("maps.ejs", mapData);
